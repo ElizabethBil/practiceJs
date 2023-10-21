15352ecb5d100b9492fec58b0172150bec85784c
TetiZ <tommytaba99@gmail.com>
2023-10-21 10:02:30 +0200

    task 2

--- js/less1.js
+++ js/less1.js
@@ -1,10 +1,21 @@
-// ЗАДАЧА 1
-// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+// // ЗАДАЧА 1
+// // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
 
-const name = "ГАЛИНА";
-const age = 35;
-const hobby = "JAVASCRIPT";
+// const name = "ГАЛИНА";
+// const age = 35;
+// const hobby = "JAVASCRIPT";
 
-const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`;
+// const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`;
 
+// console.log(message);
+
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ, ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ
+// "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+const userName = prompt("Please type your name");
+const userEmail = prompt("Please type your e-mail");
+const userTel = prompt("Please type your phone");
+
+const message = `Користувач ${userName} використовує ${userEmail} почту і ${userTel} телефон`;
 console.log(message);
