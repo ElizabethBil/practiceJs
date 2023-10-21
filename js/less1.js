30b01b715ab54c61176b22925362a7d7a4950146
ElizabethBil <140847451+ElizabethBil@users.noreply.github.com>
2023-10-21 09:54:52 +0200

    1

---
+++ js/less1.js
@@ -0,0 +1,10 @@
+// ЗАДАЧА 1
+// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+
+const name = "ГАЛИНА";
+const age = 35;
+const hobby = "JAVASCRIPT";
+
+const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`;
+
+console.log(message);
