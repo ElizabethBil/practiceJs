e61ced19919a754d69d99b9bf3ddce3761f8b3a1
serhiyserhiychuk <141840178+serhiyserhiychuk@users.noreply.github.com>
2023-10-21 11:13:32 +0300

    task 3

--- js/less1.js
+++ js/less1.js
@@ -13,9 +13,18 @@
 // ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ, ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ
 // "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
 
-const userName = prompt("Please type your name");
-const userEmail = prompt("Please type your e-mail");
-const userTel = prompt("Please type your phone");
+// const userName = prompt("Please type your name");
+// const userEmail = prompt("Please type your e-mail");
+// const userTel = prompt("Please type your phone");
 
-const message = `Користувач ${userName} використовує ${userEmail} почту і ${userTel} телефон`;
-console.log(message);
+// const message = `Користувач ${userName} використовує ${userEmail} почту і ${userTel} телефон`;
+// console.log(message);
+
+// ЗАДАЧА 3
+// ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
+
+const TVsum = Number(prompt("Please type TV price"));
+const phone = +prompt("Please type phone price");
+const tablet = +prompt("Please type tablet price");
+const totalPrice = TVsum + phone + tablet;
+console.log(totalPrice);
