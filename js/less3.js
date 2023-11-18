9ee2a10e51fb065db6db04a55701bf06ecd1d755
Topres05 <itishnik05@gmail.com>
2023-11-18 11:23:57 +0200

    Update less3.js

--- js/less3.js
+++ js/less3.js
@@ -4,13 +4,32 @@
 // greet(name) - колбек , який приймає імя і логінує в консоль рядок  "Привіт" + імя
 
 
-function letMeSeeYourName(callback) {
-    const name = prompt("Input your name:");
-    callback(name)
+// function letMeSeeYourName(callback) {
+//     const name = prompt("Input your name:");
+//     callback(name)
+// }
+
+// function greet(name) {
+//     console.log(`Hallo, ${name}`)
+// }
+
+// letMeSeeYourName(greet);
+
+// ЗАДАЧА 2
+// Напишіть дві функції:
+// makeProduct(name, price, callback) -приймає імя та ціну товару, а також колбек.Функція створює обєкт товару, добавляє йому унікальний ідентифікатор, в значення id і викликає колбек, передаючи йому створений обєкт.
+// showProduct(product) - колбек приймає обєкт продукта і логінує його в консоль;
+// makeProduct("Холодильник", 10000, logger);
+
+function makeProduct(name, price, callback) {
+    const products = { name, price, id: Date.now() }
+    callback(products)
 }
 
-function greet(name) {
-    console.log(`Hallo, ${name}`)
+function showProduct(products) {
+    console.log(products)
 }
 
-letMeSeeYourName(greet);
+// makeProduct("Холодильник", 10000, showProduct);
+
+makeProduct("Телевізор", 10000, showProduct);
